172bb0ede237b7c3a9c7134d7a41eeafac1fcfe9
wiggnash <vigneshwar.sigamani@gmail.com>
2025-10-10 09:20:37 +0530

    understand to update from string to array and array to string

--- 4_loopsAndArrays.js
+++ 4_loopsAndArrays.js
@@ -66,3 +66,21 @@ let a = 5;
 let b = a;
 a = 10;
 console.log(`a : ${a} and b is ${b}`);
+
+let numsArray = [4, 1, 5, 7, 9, 15, 11];
+numsArray.sort();
+console.log(numsArray);
+
+// searching
+let searchNumsArray = [4, 1, 5, 7, 9, 15, 11];
+// 1. search using loop
+let searchElement = 5;
+for (let i = 0; i < searchNumsArray.length; i++) {
+  if (searchNumsArray[i] === searchElement) {
+    console.log("Element found");
+  }
+}
+
+// 2. include method
+console.log(searchNumsArray.includes(searchElement));
+console.log(searchNumsArray.includes(searchElement, 3));
