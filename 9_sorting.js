0603ec240ca564b0a7ace0dcb114389e917679eb
wiggnash <vigneshwar.sigamani@gmail.com>
2025-10-23 00:03:20 +0530

    inbuilt array methods using callbacks

---
+++ 9_sorting.js
@@ -0,0 +1,7 @@
+let unSortedName = ["Vigneshwar", "Dinesh Kumar", "Surya Kumar", "Mullet"];
+unSortedName.sort();
+console.log(unSortedName);
+
+let unSortedNumbers = [1, 4, 2, 3, 0, 5, 10, 7, 11];
+unSortedNumbers.sort((a, b) => a - b);
+console.log(unSortedNumbers);
